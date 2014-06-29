0ad2b184fa90587c09c09a4df68dd5426a3d0cba
Toni Suominen <suominentoni@gmail.com>
2014-06-29 23:10:59 +0300

    Complete exercise 11 (my-drop-while function).

--- src/recursion.clj
+++ src/recursion.clj
@@ -62,7 +62,13 @@
       '()))
 
 (defn my-drop-while [pred? a-seq]
-  [:-])
+  (cond
+    (empty? a-seq)
+      '()
+    (pred? (first a-seq))
+      (my-drop-while pred? (rest a-seq))
+    :else
+      a-seq))
 
 (defn seq= [a-seq b-seq]
   :-)
